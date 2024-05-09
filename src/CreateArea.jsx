680dfa9afc6e21d60168c5b2833077cb11e602c0
Coding-byKS <khushboo.singh43@gmail.com>
2024-05-09 15:17:13 +0100

    deploy config

---
+++ src/CreateArea.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  const [hidden, setHidden] = useState(true);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  function expand() {
+    setHidden(false);
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {hidden ? null : (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={hidden ? "1" : "3"}
+        />
+        {hidden ? null : (
+          <Zoom in={true}>
+            <Fab onClick={submitNote}>
+              <AddIcon />
+            </Fab>
+          </Zoom>
+        )}
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightIcon from "@mui/icons-material/Highlight";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <LocalLibraryIcon />
+        KeeperApp
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
